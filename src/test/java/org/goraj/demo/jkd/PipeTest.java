a4393ea448e7af4d2f8e3ce252af6b98c46c8dd3
Piotr Gorajski <piotr.gorajski@gmail.com>
2019-05-29 08:18:28 +0200

    Refactor Pipe test

--- src/test/java/org/goraj/demo/jkd/PipeTest.java
+++ src/test/java/org/goraj/demo/jkd/PipeTest.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.TopologyTestDriver;
 import org.apache.kafka.streams.test.ConsumerRecordFactory;
 import org.apache.kafka.streams.test.OutputVerifier;
@@ -14,16 +15,21 @@ import java.util.Properties;
 public class PipeTest {
 
     @Test
-    public void shouldWork() {
+    public void shouldCreateTopology() {
+        //given
         Properties configuration = new Properties();
         configuration.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "pipe-test");
         configuration.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "fake:1234");
+        Topology topology = new Pipe().createTopology();
+        try (TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology, configuration)) {
+            //when
+            ConsumerRecordFactory<String, String> factory = new ConsumerRecordFactory<>("streams-plaintext-input", new StringSerializer(), new StringSerializer());
+            String inputValue = "this is value";
+            topologyTestDriver.pipeInput(factory.create(inputValue));
 
-        TopologyTestDriver topologyTestDriver = new TopologyTestDriver(new Pipe().createTopology(), configuration);
-        ConsumerRecordFactory<String, String> factory = new ConsumerRecordFactory<>("streams-plaintext-input", new StringSerializer(), new StringSerializer());
-        topologyTestDriver.pipeInput(factory.create("this is value"));
-        ProducerRecord<String, String> outputRecord = topologyTestDriver.readOutput("streams-plaintext-output", new StringDeserializer(), new StringDeserializer());
-        OutputVerifier.compareValue(outputRecord, "this is value");
-        topologyTestDriver.close();
+            //then
+            ProducerRecord<String, String> outputRecord = topologyTestDriver.readOutput("streams-plaintext-output", new StringDeserializer(), new StringDeserializer());
+            OutputVerifier.compareValue(outputRecord, inputValue);
+        }
     }
 }
