35a45dcf001d562b07d6f84c31cc227b61e139a4
Piotr Gorajski <piotr.gorajski@gmail.com>
2019-05-30 08:26:22 +0200

    Fix broken Line Split test

--- src/test/java/org/goraj/demo/jkd/LineSplitTest.java
+++ src/test/java/org/goraj/demo/jkd/LineSplitTest.java
@@ -1,6 +1,7 @@
 package org.goraj.demo.jkd;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.streams.StreamsConfig;
@@ -12,6 +13,8 @@ import org.junit.Test;
 
 import java.util.Properties;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LineSplitTest {
 
     @Test
@@ -20,17 +23,24 @@ public class LineSplitTest {
         Properties configuration = new Properties();
         configuration.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "line-split-test");
         configuration.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "fake:1234");
+        configuration.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        configuration.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
         Topology topology = new LineSplit().createTopology();
         try (TopologyTestDriver topologyTestDriver = new TopologyTestDriver(topology, configuration)) {
             //when
             ConsumerRecordFactory<String, String> factory = new ConsumerRecordFactory<>("streams-plaintext-input", new StringSerializer(), new StringSerializer());
-            // FIXME: It does not work on java 11 due to:
-            // java.lang.ClassCastException: class [B cannot be cast to class java.lang.String ([B and java.lang.String are in module java.base of loader 'bootstrap')
             topologyTestDriver.pipeInput(factory.create("this is value"));
 
             //then
-            ProducerRecord<String, String> outputRecord = topologyTestDriver.readOutput("streams-line-split-output", new StringDeserializer(), new StringDeserializer());
-            OutputVerifier.compareValue(outputRecord, "this is value");
+            OutputVerifier.compareValue(readOutput(topologyTestDriver), "this");
+            OutputVerifier.compareValue(readOutput(topologyTestDriver), "is");
+            OutputVerifier.compareValue(readOutput(topologyTestDriver), "value");
+            assertThat(readOutput(topologyTestDriver)).isNull();
         }
+    }
+
+    private ProducerRecord<String, String> readOutput(TopologyTestDriver topologyTestDriver) {
+        return topologyTestDriver.readOutput("streams-line-split-output", new StringDeserializer(), new StringDeserializer());
     }
 }
